cc33e146f9a3a5c08ed01863cd4cb773bc274f30
janosheen <91180541+janosheen@users.noreply.github.com>
2021-10-31 15:11:02 +0100

    I Implemented the method  that checks, if a cake can be divided fairly between people.

--- Open-Lab-03.07/Divider.cs
+++ Open-Lab-03.07/Divider.cs
@@ -6,7 +6,14 @@ namespace Open_Lab_03._07
     {
         public bool EqualSlices(int total, int people, int each)
         {
-            throw new NotImplementedException();
+            if (total % each == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
